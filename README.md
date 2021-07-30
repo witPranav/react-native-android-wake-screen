b8bd494fe50b709f196b052f648cd9072aeaa0de
Pranav Chatly <pchatly786@gmail.com>
2021-07-30 14:44:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-native-android-wake-screen
+
+## Getting started
+
+`$ npm install react-native-android-wake-screen --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-android-wake-screen`
+
+## Usage
+```javascript
+import AndroidWakeScreen from 'react-native-android-wake-screen';
+
+// TODO: What to do with the module?
+AndroidWakeScreen;
+```
---
+++ android/src/main/java/com/reactlibrary/AndroidWakeScreenModule.java
@@ -0,0 +1,37 @@
+// AndroidWakeScreenModule.java
+
+package com.reactlibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import android.os.PowerManager;
+import android.os.Build;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+
+public class AndroidWakeScreenModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public AndroidWakeScreenModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "AndroidWakeScreen";
+    }
+
+    @ReactMethod
+    public void wakeScreen() {
+        PowerManager powerManager = (PowerManager) getReactApplicationContext().getSystemService(getReactApplicationContext().POWER_SERVICE);
+        boolean screenOn = Build.VERSION.SDK_INT >= 20 ? powerManager.isInteractive() : powerManager.isScreenOn();
+        if (!screenOn) {
+            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "myApp:notificationLock");
+            wakeLock.acquire();
+        }
+    }
+}
